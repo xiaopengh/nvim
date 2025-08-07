bd33e24a8a792dd6f96f2f8e37ce374b25b3d4bb
xiaopengh <mathys.zhang@yahoo.com>
2025-08-07 17:32:23 +0200

    set visual selection

--- lua/LuaSnip/all.lua
+++ lua/LuaSnip/all.lua
@@ -1,16 +1,24 @@
 -- ~/.config/nvim/lua/LuaSnip/all.lua
 
-return {
-  -- A snippet that expands the trigger "hi" into the string "Hello, world!".
-  require("luasnip").snippet(
-    { trig = "hi" },
-    { t("Hello, world!") }
-  ),
 
-  -- To return multiple snippets, use one `return` statement per snippet file
-  -- and return a table of Lua snippets.
-  require("luasnip").snippet(
-    { trig = "foo" },
-    { t("Another snippet.") }
-  ),
+return {
+	-- A snippet that expands the trigger "hi" into the string "Hello, world!".
+	require("luasnip").snippet(
+	{ trig = "hi" },
+	{ t("Hello, world!") }
+	),
+
+	-- To return multiple snippets, use one `return` statement per snippet file
+	-- and return a table of Lua snippets.
+	require("luasnip").snippet(
+	{ trig = "foo" },
+	{ t("Another snippet.") }
+	),
+
+	-- Set the key to trigger visual selection
+	require("luasnip").config.set_config({ -- Setting LuaSnip config
+	  -- Use <Tab> (or some other key if you prefer) to trigger visual selection
+	  store_selection_keys = "<Tab>",
+	})
+
 }
