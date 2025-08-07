e159485366150bc9405f4aa13ef68d2f0bea273b
xiaopengh <mathys.zhang@yahoo.com>
2025-08-07 16:17:36 +0200

    update basic settings

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -3,6 +3,18 @@
 ---------------------------------------------------
 -- Basic settings
 ---------------------------------------------------
+-- Set relative and absolute line numbers
+vim.opt.number = true         -- Show absolute line number on the current line
+vim.opt.relativenumber = true -- Show relative numbers on other lines
+
+-- Set indentation options
+vim.opt.tabstop = 4           -- Number of visual spaces per TAB
+vim.opt.shiftwidth = 4        -- Number of spaces to use for each step of (auto)indent
+vim.opt.softtabstop = 4       -- Number of spaces a <Tab> feels like during editing
+vim.opt.expandtab = false      -- Convert tabs to spaces
+
+
+-- Set local leader
 vim.g.mapleader = "\\"
 
 -- Quick save keybindings (Ctrl+s)
