a254dff9484fbfa1b9aec9f5d11dba7dc22a7e42
xiaopengh <mathys.zhang@yahoo.com>
2025-08-08 14:49:16 +0200

    add formating

---
+++ lua/LuaSnip/tex/formatting.lua
@@ -0,0 +1,44 @@
+-- Abbreviations used in this article and the LuaSnip docs
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
+-- ----------------------------------------------------------------------------
+-- Summary: When `LS_SELECT_RAW` is populated with a visual selection, the function
+-- returns an insert node whose initial text is set to the visual selection.
+-- When `LS_SELECT_RAW` is empty, the function simply returns an empty insert node.
+local get_visual = function(args, parent)
+  if (#parent.snippet.env.LS_SELECT_RAW > 0) then
+    return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
+  else  -- If LS_SELECT_RAW is empty, return a blank insert node
+    return sn(nil, i(1))
+  end
+end
+-- ----------------------------------------------------------------------------
+
+return {
+-- Example: italic font implementing visual selection
+s({trig = "tii", dscr = "Expands 'tii' into LaTeX's textit{} command."},
+  fmta("\\textit{<>}",
+    {
+      d(1, get_visual),
+    }
+  )
+),
+
+s({ trig = "tbb", dscr = "Expands 'tbb' into LaTeX's textbf{} command." },
+  fmta("\\textbf{<>}",
+    {
+      d(1, get_visual),
+    }
+  )
+)
+
+}
